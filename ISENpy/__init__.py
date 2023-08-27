e1ec56e210c858e26e8022b3fccb3a6267ee781d
CorentinMre <corentin.marie@laposte.net>
2023-08-27 19:05:06 +0200

    upgrade version

--- ISENpy/__init__.py
+++ ISENpy/__init__.py
@@ -23,7 +23,7 @@ __title__ = "ISENpy"
 __author__ = "CorentinMre"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) CorentinMre"
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 
 
 from .dataClasses import *

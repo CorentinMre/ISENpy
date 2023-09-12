6108c5623fa7c7a9c1acd056d27d4eca173fa81c
CorentinMre <corentin.marie@laposte.net>
2023-09-12 20:34:16 +0200

    upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ISENpy',
-    version='0.5.4',    
+    version='0.5.6',    
     description='A python API wrapper for ISEN-OUEST',
     long_description_content_type = "text/markdown",
     long_description=long_description,
